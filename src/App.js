8d1dfe038ae479fae576175692a5a8153202629c
Aime Comeault <67168818+Aimcomlt@users.noreply.github.com>
2021-10-23 01:06:30 -0600

    optomized...

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import { Line } from 'react-chartjs-2';
 //import { Bar } from 'react-chartjs-2';
 import { useDispatch, useSelector } from "react-redux";
 import { getData } from "./actions/bitcoinActions";
+import { mailBox } from "./actions/bitcoinActions";
+
 //import 'chartjs-adapter-luxon';
 //import background from "./gallery/High_resolution.jpeg";
 import ChartStreaming from 'chartjs-plugin-streaming';
@@ -17,6 +19,111 @@ function App() {
   const stateB = useSelector(state => state.brain)
   const [num] = React.useState(500);
   const [mom] = React.useState(0.1); 
+
+    ////////////////////////////////////////////
+    const percentMoveO = [];
+    const MoveO = [];
+    const percentMoveH = [];
+    const MoveH = [];
+    const percentMoveL = [];
+    const MoveL = [];
+    const percentMoveC = [];
+    const MoveC = [];
+
+  const [initA] = React.useState(100);
+/*
+  const percA = [];
+  percA.push(percentMoveO)
+  /*
+  function foo() {
+    return percA.push(0)
+  }
+  foo()
+  
+ // console.log(openPer)
+  for(let i = 0; i < percA.length; i++){
+    var openPer = percA[i];
+    if(percentMoveO > 1) {percA.push(openPer + percentMoveO)};
+    if(percentMoveO < 1) {percA.push(openPer - percentMoveO)};
+  }
+
+
+  const percB = [];
+  var highPer = percB;
+  if(percentMoveH >= 1) {percB.push(highPer + percentMoveH)}else{percB.push(highPer - percentMoveH)};
+
+  const percC = []; 
+  var lowPer = percC;
+  if(percentMoveL >= 1) {percC.push(lowPer + percentMoveL)}else{percC.push(lowPer - percentMoveL)};
+
+  const percD = []; 
+  var closePer = percD;
+  if(percentMoveC >= 1) {percD.push(closePer + percentMoveC)}else{percD.push(closePer - percentMoveC)};
+
+console.log(percA, percB, percC, percD)
+*/
+
+ const openV = initA / 4;
+  const highV = initA / 4;
+  const lowV = initA / 4;
+  const closeV = initA / 4;
+ 
+
+  //console.log(openV, highV, lowV, closeV)
+  //const [inMarketAmount, setNewAmount] = React.useState(openV);
+  const [InitAmountopen, setInitAmountOP] = React.useState(() => initA / 4);
+  const [InitAmounthigh, setInitAmountHGH] = React.useState(() => initA / 4);
+  const [InitAmountlow, setInitAmountLW] = React.useState(() => initA / 4);
+  const [InitAmountclose, setInitAmountCL] = React.useState(() => initA / 4);
+  const [SupplyA, setSupplyA] = React.useState(100);
+  const [SupplyB, setSupplyB] = React.useState(100);
+  const [SupplyC, setSupplyC] = React.useState(100);
+  const [SupplyD, setSupplyD] = React.useState(100);  
+  /*
+let ITO = (InitAmountopen);
+let ITH = (InitAmounthigh);
+let ITL = (InitAmountlow)
+let ITC = (InitAmountclose);
+  var allResult =(((ITO + ITH) + ITL) + ITC);
+  console.log((((ITO + ITH) + ITL) + ITC)) 
+  const [dai, setDai] = React.useState(allResult);
+*/
+
+  
+  for(let i = 0; i < mailBox[0].length; i++) {
+    MoveO.push(mailBox[0][499 + i] / mailBox[0][498 + i]);
+      }
+ percentMoveO.push(MoveO[mailBox[0].length - 500]);
+  //console.log(percentMoveO)
+/////////////////////////////////////////////////////////
+for(let i = 0; i < mailBox[1].length; i++) {
+  MoveH.push(mailBox[1][499 + i] / mailBox[1][498 + i]);
+    }
+percentMoveH.push(MoveH[mailBox[1].length - 500])
+/////////////////////////////////////////////////////////
+for(let i = 0; i < mailBox[2].length; i++) {
+  MoveL.push(mailBox[2][499 + i] / mailBox[2][498 + i]);
+    }
+percentMoveL.push(MoveL[mailBox[2].length - 500])
+////////////////////////////////////////////////////////
+for(let i = 0; i <mailBox[3].length; i++) {
+  MoveC.push(mailBox[3][499 + i] / mailBox[3][498 + i]);
+    }
+percentMoveC.push(MoveC[mailBox[3].length - 500]);
+
+
+const dataOpen = [];
+dataOpen.push(((initA / 4) / SupplyA) * percentMoveO);
+const dataHigh = [];
+dataHigh.push(((initA / 4) / SupplyB) * percentMoveH) 
+const dataLow = [];
+dataLow.push(((initA / 4) / SupplyC) * percentMoveL) 
+const dataClose = [];
+dataClose.push(((initA / 4) / SupplyD) * percentMoveC) 
+const dataTotal = [];
+dataTotal.push((dataOpen) + (dataHigh) + (dataLow) + (dataClose))
+console.log(dataTotal)
+ // const [mint] = React.useState(0.1 * Supply)
     
   const fetchData = (time) => {
     //Fetch data from redux using time
@@ -24,8 +131,19 @@ function App() {
       time: time,
       number: num,
       momentum: mom,
+    //  NewAmount: setNewAmount,
+      initAmountOP: setInitAmountOP,
+      initAmountHGH: setInitAmountHGH,
+      initAmountLW: setInitAmountLW,
+      initAmountCL: setInitAmountCL,
+      maxSupplyA: SupplyA,
+      maxSupplyB: SupplyB,
+      maxSupplyC: SupplyC,
+      maxSupplyD: SupplyD,
+    //  Dai: setDai,
     }))
   }
+ // console.log(TempXopen);
 ///<input onChange={e => setNum(e.target.value)} />https://ipfs.io/ipfs/QmeMgBEWFGgQKjmz8CvVLGQuU32rT9zEykhiY4AfhEpckB?filename=Eye-Dragon-Wing2.png
 return (
   <div className="App">
@@ -47,8 +165,13 @@ return (
     <h4>This project is build out of passion for big Data analytic</h4>
     <h4>In other words extremely large data sets that may be analyzed computationally to reveal patterns, trends, and associations, especially relating to human behavior and interactions.</h4>
     <h4>This is were the experimental part comes in.</h4>
-    <h4>The question is when to execute trigger(s) speculating an up trend or speculating a down trend.</h4>
-    <h4>Is it 🧂salt or is it 🌶️pepper?</h4>  
+    <h4>For years the common stock invester strategy was buy low sell high and some other factors abviously. But the era is now and decentralized networks are changing the map.</h4>
+    <h4>The road map for this app is to introduce a neural network to compute behaviour and use the network to simply act like a mirror. We compare the asset verses the asset </h4>
+    <h4>We compare the asset verses the asset and this outputs a uniq signature.</h4>
+    <h4>Then this signature can be used to compare other assets with precision</h4>
+    <h4>How we bring precision is to create four mock tokens A B C D and we pair there value with the open high low close value individualy.</h4>
+    <h4>Theses four tokens became the signature and the signature is expresss in a regular price chart</h4>
+   <h4>Is it 🧂salt or is it 🌶️pepper?</h4>  
   <h1>◻️Word from the developer◻️</h1>
     <h4>This is were the magic🪄 begins, using brain.js neural network to predict trends📈. But truly were to start?🤔</h4>
     <h4>Working with brain js teleported me back 44 years ago, when I could just understand that mixing lots of colors together equals black.</h4> 
@@ -110,14 +233,85 @@ return (
   
   <h4>OPEN : RED🔴 | HIGH : BLUE🔵 | LOW : YELLOW🟡 | CLOSE : GREEN🟢</h4>
  
-  <button className='btns-wrapperA' onClick={() => setInterval(() => {fetchData("momentum")}, 60100)}>⏳START 1 MIN INTERVAL</button>
-          <button className='btns-wrapperB' onClick={(() => {fetchData()})}>🕳️INSERT TRAINNING DATA📃📃... NOW🕳️</button>
+
           <h4>The Training array snapshot📸.</h4>
           <Line className='snap-shot' 
-         data={state.data} options={{responsive: true}}
+           data={state.data} options={{responsive: true}}
          />
+         <div className='tinker-minter'>
+           <h1 className='tinker-title'>
+             THE TINKER MINTER DAI EXTRACTOR
+           </h1>
+           <h3 className='tinker-title'>
+             (TEST VERSION 1.0.0)
+           </h3>
+         <button className='btns-wrapperA' onClick={() => setInterval(() => {fetchData("momentum")}, 60100)}>⏳START 1 MIN INTERVAL</button>
+           <button className='btns-wrapperB' onClick={(() => {fetchData()})}>🕳️INSERT TRAINNING DATA📃📃... NOW🕳️</button>
+
+           <div className='init-amount-backg'>
+
+           <div className='initial-Amount'>
+           <h1>🧐</h1>
+           <h4>SPECULATIVE MOCK DAI ACCOUNT: {(InitAmountopen * percentMoveO) + (InitAmounthigh * percentMoveH) + (InitAmountlow * percentMoveL) + (InitAmountclose * percentMoveC)}</h4>
+           </div>
+
+           </div>
+
+      <div className='moch-tokens' >
+      <div className='maxSupA'>
+           <input onChange={e => setSupplyA(e.target.value)} />
+           <h1>🪙</h1>
+           <h4>MOCK TOKEN A MAX SUPPLY : {SupplyA}</h4>
+           <input onChange={e => setInitAmountOP(e.target.value)} />
+           <h4>PRICE PER UNIT : {InitAmountopen / SupplyA} </h4>
+           <h4> PERCENT % MOVE : {percentMoveO}</h4>
+           <h4>DAI BALANCE : {InitAmountopen * percentMoveO}</h4>
+           </div>
+
+           <div className='maxSupB'>
+           <input onChange={e => setSupplyB(e.target.value)} />
+           <h1>🪙</h1>
+           <h4>MOCK TOKEN B MAX SUPPLY : {SupplyB}</h4>
+           <input onChange={e => setInitAmountHGH(e.target.value)} />
+           <h4> PRICE PER UNIT : {InitAmounthigh / SupplyB} </h4>
+           <h4> PERCENT % MOVE : {percentMoveH}</h4>
+           <h4> DAI BALANCE : {InitAmounthigh * percentMoveH}</h4>
+           </div>
+
+           <div className='maxSupC'>
+           <input onChange={e => setSupplyC(e.target.value)} />
+           <h1>🪙</h1>
+           <h4>MOCK TOKEN C MAX SUPPLY : {SupplyC}</h4>
+           <input onChange={e => setInitAmountLW(e.target.value)} />
+           <h4>PRICE PER UNIT : {InitAmountlow / SupplyC}</h4>
+           <h4> PERCENT % MOVE : {percentMoveL}</h4>
+           <h4> DAI BALANCE : {InitAmountlow * percentMoveL}</h4>
+           </div>
+
+           <div className='maxSupD'>
+           <input onChange={e => setSupplyD(e.target.value)} />
+           <h1>🪙</h1>
+           <h4>MOCK TOKEN D MAX SUPPLY : {SupplyD}</h4>
+           <input onChange={e => setInitAmountCL(e.target.value)} />
+           <h4>PRICE PER UNIT : {InitAmountclose / SupplyD}</h4>
+           <h4> PERCENT % MOVE : {percentMoveC}</h4>
+           <h4>DAI BALANCE : {InitAmountclose * percentMoveC}</h4>
+           </div>
+      </div>
+      <div className='dai-amount-backg'>
+           <div className='dai-Amount'>    
+           <h1>🪙</h1>
+           <h4>MOCK DAI ACCOUNT: {(InitAmountopen * percentMoveO) + (InitAmounthigh * percentMoveH) + (InitAmountlow * percentMoveL) + (InitAmountclose * percentMoveC)}</h4>
+           </div>
+
+           </div>
+         </div>
+
+
+
+     
          <div className= 'projects-link' >
-         <img src ={'https://ipfs.io/ipfs/QmTTabZTHf7ejwTJxUwNR9g15aCV9Q65yD5UvdQb7qfwMm?filename=Dragon-Head.png'} style={{ width: '300px', marginBottom: '100px', height: '350px' }} alt="adam"/>
+         <img className='the-image-projects' src ={'https://ipfs.io/ipfs/QmTTabZTHf7ejwTJxUwNR9g15aCV9Q65yD5UvdQb7qfwMm?filename=Dragon-Head.png'} alt="adam"/>
          <a className= 'the-link-projects' href={'http://crypto-flowers.surge.sh/'}> ☣️NFT-testnet-rinkeby☣️ CRYPTO-FLOWERS GALLERY</a>
          
          </div>
@@ -132,61 +326,98 @@ return (
         <div className='container'>
 
         <div className='chartA-I-result' style={{height:'60%', width:'fit-content(100)'}}>
-            <h2 align = 'center'>⚪PREDICTED OPEN⚪</h2>
+            <h2 align = 'center'>🔴PREDICTED OPEN🔴</h2>
             <h5 align = 'center'>BTC/USDT TICKER📈</h5>
             <h3> PREDICTED OPEN VERSES LATESS OPEN PRICE </h3>
-
          <Line
          data={stateB.dataOPEN} options={{responsive: true}}
          />
          </div>
+
          <div className='chartA-I-result' style={{height:'60%', width:'fit-content(100)'}}>
-            <h2 align = 'center'>⚪PREDICTED HIGH⚪</h2>
+            <h2 align = 'center'>🔴EXPERIMENTAL OPEN TRIGGER🔴</h2>
+            <h5 align = 'center'>BTC/USDT TICKER📈</h5>
+            <h3>🔴EXPERIMENTAL </h3>
+            <Line
+         data={stateB.dataTimeTwistOPEN} options={{responsive: true}}
+         />
+         </div>
+
+
+
+         <div className='chartA-I-result' style={{height:'60%', width:'fit-content(100)'}}>
+            <h2 align = 'center'>🔵PREDICTED HIGH🔵</h2>
             <h5 align = 'center'>BTC/USDT TICKER📈</h5>
             <h3>PREDICTED HIGH VERSES LATESS HIGH PRICE </h3>
-
          <Line
          data={stateB.dataHIGH} options={{responsive: true}}
          />
          </div>
+
+         
          <div className='chartA-I-result' style={{height:'60%', width:'fit-content(100)'}}>
-            <h2 align = 'center'>⚪PREDICTED LOW⚪</h2>
+            <h2 align = 'center'>🔵EXPERIMENTAL HIGH TRIGGER🔵</h2>
+            <h5 align = 'center'>BTC/USDT TICKER📈</h5>
+            <h3>🔵EXPERIMENTAL</h3>
+            <Line
+         data={stateB.dataTimeTwistHIGH} options={{responsive: true}}
+         />
+         </div>
+
+
+
+         <div className='chartA-I-result' style={{height:'60%', width:'fit-content(100)'}}>
+            <h2 align = 'center'>🟡PREDICTED LOW🟡</h2>
             <h5 align = 'center'>BTC/USDT TICKER📈</h5>
             <h3> PREDICTED LOW VERSES LATESS LOW PRICE</h3>
-
          <Line
          data={stateB.dataLOW} options={{responsive: true}}
          />
          </div>
-         <div className='chartA-I-result' style={{height:'60%', width:'fit-content(100)'}}>
-            <h2 align = 'center'>⚪PREDICTED CLOSE⚪</h2>
-            <h5 align = 'center'>BTC/USDT TICKER📈</h5>
-            <h3>PREDICTED CLOSE VERSES LATESS CLOSE PRICE </h3>
 
+
+
+
+         <div className='chartA-I-result' style={{height:'60%', width:'fit-content(100)'}}>
+            <h2 align = 'center'>🟡EXPERIMENTAL LOW TRIGGER🟡</h2>
+            <h5 align = 'center'>BTC/USDT TICKER📈</h5>
+            <h3>🟡EXPERIMENTAL</h3>
+            <Line
+         data={stateB.dataTimeTwistLOW} options={{responsive: true}}
+         />
+         </div>
+
+
+           <div className='chartA-I-result' style={{height:'60%', width:'fit-content(100)'}}>
+            <h2 align = 'center'>🟢PREDICTED CLOSE🟢</h2>
+            <h5 align = 'center'>BTC/USDT TICKER📈</h5>
+            <h3>EXPERIMENTAL TRIGGER LINES CONTINUE... </h3>
          <Line
          data={stateB.dataCLOSE} options={{responsive: true}}
          />
          </div>
 
-           <div className='chartA-I-result' style={{height:'60%', width:'fit-content(100)'}}>
-            <h2 align = 'center'>⚪MESUREMENT OF THE TREND⚪</h2>
+
+         <div className='chartA-I-result' style={{height:'60%', width:'fit-content(100)'}}>
+            <h2 align = 'center'>🟢EXPERIMENTAL CLOSE TRIGGER🟢</h2>
             <h5 align = 'center'>BTC/USDT TICKER📈</h5>
-            <h3> CHART NUMBER #1 </h3>
+            <h3>🟢EXPERIMENTAL</h3>
+
+         <Line
+         data={stateB.dataTimeTwist} options={{responsive: true}}
+         />
+         </div>
+
+           <div className='chartA-I-result' style={{height:'60%', width:'fit-content(100)'}}>
+            <h2 align = 'center'>🔴🔵🟡🟢EXPERIMENTAL OPEN HIGH LOW CLOSE TRIGGER🟢🟡🔵🔴</h2>
+            <h5 align = 'center'>BTC/USDT TICKER📈</h5>
+            <h3>EXPERIMENTAL TRIGGER LINES CONTINUE... </h3>
 
          <Line
          data={stateB.dataG} options={{responsive: true}}
          />
          </div>
 
-        <div className='chartB-II-result' style={{height:'60%', width:'fit-content(100)'}}>
-           <h2 align = 'center'>⚪THE PRICE FORCE AND TREND DIRECTION⚪</h2>
-           <h5 align = 'center'>BTC/USDT TICKER📈</h5>
-           <h3> CHART NUMBER #2 </h3>
-
-         <Line
-         data={stateB.dataH} options={{responsive: true}}
-         />
-         </div>
 
            <div className='chartC-III-result' style={{height:'60%', width:'fit-content(100)'}}>
            <h2 align = 'center'>⚪THE LATESS PRICE VERSES THE PREDICTOR⚪</h2>
@@ -208,16 +439,6 @@ return (
          />
          </div>
 
-         <div className='chartE-V-result' style={{height:'60%', width:'fit-content(100)'}}>
-            <h2 align = 'center'>⚪THE PREDICTOR CORE AVERAGE🟠 OF ALL NEURAL NETWORKS O-H-L-C⚪</h2>
-            <h5 align = 'center'>BTC/USDT TICKER📈</h5>
-            <h3> CHART NUMBER #5 </h3>
-  
-         <Line 
-         data={stateB.dataF} options={{responsive: true}}
-         />
-         </div>
-
           <div className='chartF-VI-result' style={{height:'60%', width:'fit-content(100)'}}>
             <h2 align = 'center'>⚪BRAIN PREDICTOR LAYER@2.0.0⚪</h2>
             <h5 align = 'center'>BTC/USDT TICKER📈</h5>
@@ -236,13 +457,13 @@ return (
          /></div>
 
          </div>
-         <div align="left" style={{ width: '100%', marginTop: '52%', height: '1%', backgroundColor: 'rgba(3, 0, 0, 0.6)' }} >
+         <div className='bottom-nav' >
           <img src ={'https://ipfs.io/ipfs/QmTTabZTHf7ejwTJxUwNR9g15aCV9Q65yD5UvdQb7qfwMm?filename=Dragon-Head.png'} href={'https://ipfs.io/ipfs/QmWRdonnmgFYUZEUUAQzenp2jeCyRiVkq8Ryp5H49mFrpz'} style={{ width: '385px', marginTop: '3%', height: '400px' }} alt="adam" button here/>
           <div>
            <h1 align='center' style={{marginTop: '75px'}}>Road Map</h1>
            <h4 align='center'>The projet concists of targeting a fair price of any asset leading to take advantage when price is temporarly depressed</h4>
            </div>
-          <div align="center" style={{ width: '60%', height: '50%', marginBottom: '1%', backgroundColor: 'rgba(3, 0, 0, 0.1)' }}>
+          <div align="center" style={{ width: '60%', height: '50%', marginLeft: '20%', marginBottom: '1%', backgroundColor: 'rgba(3, 0, 0, 0.1)' }}>
         <h1 style={{height : '150px'}}> 
                   <a  align="center" href={'https://discord.gg/9HYr36dF'} rel="noopener noreferrer" style={{color: "#black"}}>
                                   MY DISCORD SERVER
@@ -279,6 +500,14 @@ SOME STUFF:
              
              </div>
  <input onChange={e => setNum(e.target.value)} />
+        <div className='chartB-II-result' style={{height:'60%', width:'fit-content(100)'}}>
+           <h2 align = 'center'>⚪THE PRICE FORCE AND TREND DIRECTION⚪</h2>
+           <h5 align = 'center'>BTC/USDT TICKER📈</h5>
+           <h3> CHART NUMBER #2 </h3>
 
+         <Line
+         data={stateB.dataH} options={{responsive: true}}
+         />
+         </div>
 
 */
